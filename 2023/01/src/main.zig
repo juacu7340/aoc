93d27b1807657ecfcb611cfef1aaa821f4b1fd16
Júlio Pacheco <juliodryfish@protonmail.com>
2023-12-01 22:12:50 +0000

    2023 01

---
+++ 2023/01/src/main.zig
@@ -0,0 +1,50 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
+    const file = try std.fs.cwd().openFile("sample.txt", .{});
+    defer file.close();
+
+    var counter: usize = 0;
+    while (true) {
+        var line_buffer = std.ArrayList(u8).init(allocator);
+        defer line_buffer.deinit();
+
+        file.reader().streamUntilDelimiter(line_buffer.writer(), '\n', null) catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
+
+        var items = line_buffer.items;
+
+        for (items) |value| {
+            if (value >= '0' and
+                value <= '9')
+            {
+                counter += (value - '0') * 10;
+                break;
+            }
+        }
+
+        var reverse_index: usize = items.len;
+        while (reverse_index > 0) {
+            reverse_index -= 1;
+
+            if (items[reverse_index] >= '0' and
+                items[reverse_index] <= '9')
+            {
+                counter += items[reverse_index] - '0';
+                break;
+            }
+        }
+    }
+
+    std.debug.print("{d}\n", .{counter});
+}
